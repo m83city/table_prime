dab8623dac9e5952cc17f1c9eae3b3e22e1e8f1a
Dima <rostorguievv@gmail.com>
2021-11-30 09:35:14 +0200

    test

---
+++ src/components/Item.jsx
@@ -0,0 +1,36 @@
+
+import { InputText } from 'primereact/inputtext';
+import '../css/item.css';
+ 
+function Item (){
+
+    return(
+        <div>
+            <ul className = 'item-box'>
+                <li>
+<InputText  value = "lorem"/>
+ </li>
+                <li>
+<InputText  value = "lorem"/>
+ </li>
+                <li>
+<InputText  value = "lorem" />
+ </li>
+                <li>
+<InputText  value = "lorem"/>
+ </li>
+                <li>
+<InputText  value = "lorem"/>
+ </li>
+                <li>
+<InputText  value = "lorem"/>
+ </li>
+                <li>
+<InputText  value = "lorem"/>
+ </li>
+            </ul>
+            
+        </div>
+    );
+}
+export default Item;
